b46aa4ebd1478e69aa82df8e4555dd20fe3d44e1
Claptrap-maker <lelik575435@gmail.com>
2023-08-23 15:50:41 +0300

    Задание 2 (изменила тип возвращаемого значения метода swap на SwapNumbers).

--- src/main/java/ru/test_task/task2/SwapNumbers.java
+++ src/main/java/ru/test_task/task2/SwapNumbers.java
@@ -1,13 +1,28 @@
 package ru.test_task.task2;
 
-import javafx.util.Pair;
+import java.util.Objects;
+
 /**
  * Класс, в котором реализован метод, меняющий местами значения
  * 2ух целочисленных переменных.
  * @author IVANOVA JULIA
- * @version 2.0
+ * @version 3.0
  */
 public class SwapNumbers {
+
+    /**
+     * Хранение первого целочисленного значения в переменной типа int
+     */
+    private int firstNumber;
+    /**
+     * Хранение второго целочисленного значения в переменной типа int
+     */
+    private int secondNumber;
+
+    public SwapNumbers(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
 
     /**
      * Метод меняет местами 2 целочисленных переменных, используя XOR,
@@ -15,12 +30,29 @@ public class SwapNumbers {
      * Метод работает с числами, которые входят в диапазон от -2^31 до
      * 2^31 – 1.
      */
-    public static Pair<Integer, Integer> swap(int firstNumber, int secondNumber) {
+    public SwapNumbers swap() {
         if (firstNumber != secondNumber) {
             firstNumber ^= secondNumber;
             secondNumber = firstNumber ^ secondNumber;
             firstNumber ^= secondNumber;
         }
-        return new Pair<>(firstNumber, secondNumber);
+        return new SwapNumbers(firstNumber, secondNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SwapNumbers that = (SwapNumbers) o;
+        return firstNumber == that.firstNumber && secondNumber == that.secondNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstNumber, secondNumber);
     }
 }
