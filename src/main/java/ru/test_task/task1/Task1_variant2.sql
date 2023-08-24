bdbae80ba797d8305d5a462ec0e7f3b6a39c7776
Julia <110702344+Claptrap-maker@users.noreply.github.com>
2023-08-24 16:07:40 +0300

    Update Task1_variant2.sql
    Изменила второй запрос

--- src/main/java/ru/test_task/task1/Task1_variant2.sql
+++ src/main/java/ru/test_task/task1/Task1_variant2.sql
@@ -1,4 +1,4 @@
-SELECT surname FROM employee
-WHERE experience = (SELECT DISTINCT(experience) FROM employee as e1
-WHERE (SELECT COUNT(DISTINCT(experience))=2 FROM employee as e2
-WHERE e1.experience <= e2.experience));
+select surname from employee
+where experience = (select distinct(experience) from employee e1
+where (select count(distinct(experience)) = 2 from employee e2
+where e1.experience <= e2.experience));
